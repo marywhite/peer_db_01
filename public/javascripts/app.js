ed8f97514c776be31b7dfedcf70d1f08a65f01e2
Mary White <maryk.december@gmail.com>
2015-05-13 11:22:03 -0500

    working modify

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,5 @@
+var el;
+
 $(document).ready(function(){
 
     function getData() {
@@ -30,15 +32,45 @@ $(document).ready(function(){
         });
     });
 
+
+    $('.entries').on('click', '.update', function(){
+        $('.revise').removeClass('hidden');
+        el = $(this).parent();
+        $('.name').val(el.find('.userName').attr('id'));
+        $('.score').val(el.find('.userScore').attr('id'));
+        $('.date').val(el.find('.userDate').attr('id'));
+    });
+
+
+    $('.submit').click(function(){
+        var dataObject = { 'name': $('.name').val(), 'score': $('.score').val(), 'date_completed': $('.date_completed').val() };
+        var id = el.attr('id');
+        $.ajax({
+            url: '/assignments/' + id,
+            type: 'PUT',
+            data: JSON.stringify(dataObject),
+            contentType: 'application/json',
+            success: function(result) {
+                console.log(result);
+                getData();
+            }
+        });
+    });
+
     function display(res) {
         var entry;
         var date;
-        var update = "<button class='remove'>Remove</button>";
+        var name;
+        var score;
+        var update = "<button class='update'>Update Entry</button>";
         var button = "<button class='remove'>Remove</button>";
         for (var i = 0; i < res.length; i++){
             entry = res[i];
             date = $.trim(entry["date_completed"].substring(0, 10));
-            $('.entries').append("<li id=" + entry["_id"] +"> <b>Name: </b>" + entry["name"] + " <b>Score: </b>" + entry["score"] + " <b>Date Completed: </b>" + date + button + "</li>");
+            date = "<p class='userDate' id=" + date + "><b>Date Completed: </b>" + date + "</p>";
+            name = "<p class='userName' id=" + entry["name"] + "><b>Name: </b>" + entry["name"] + "</p>";
+            score = "<p class='userScore' id=" + entry["score"] + "><b>Score: </b>" + entry["score"] + "</p>";
+            $('.entries').append("<li id=" + entry["_id"] +">" + name + score + date + button + update +"</li>");
         }
     }
 });
